337f630e2d9e49225ed935855a8df0b44762520a
thmsn <9084377+thmsndk@users.noreply.github.com>
2020-07-25 04:59:41 +0200

    Added support for SS3

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,70 @@
 "use strict";
 
-import clear from 'rollup-plugin-clear';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from 'rollup-plugin-typescript2';
-import screeps from 'rollup-plugin-screeps';
+import clear from "rollup-plugin-clear";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import screeps from "rollup-plugin-screeps";
+import { ConfigManager } from "./ConfigManager";
 
-let cfg;
 const dest = process.env.DEST;
-if (!dest) {
-  console.log("No destination specified - code will be compiled but not uploaded");
-} else if ((cfg = require("./screeps.json")[dest]) == null) {
-  throw new Error("Invalid upload destination");
-}
 
-export default {
-  input: "src/main.ts",
-  output: {
-    file: "dist/main.js",
-    format: "cjs",
-    sourcemap: true
-  },
+const loadSS3Config = async () => {
+  // SS3 https://github.com/screepers/screepers-standards/blob/master/SS3-Unified_Credentials_File.md
+  const configManager = new ConfigManager();
+  const ss3 = await configManager.getConfig();
 
-  plugins: [
-    clear({ targets: ["dist"] }),
-    resolve(),
-    commonjs(),
-    typescript({tsconfig: "./tsconfig.json"}),
-    screeps({config: cfg, dryRun: cfg == null})
-  ]
-}
+  const server = ss3 ? ss3.servers[dest] : null;
+
+  if (!server) {
+    console.log("did not find dest");
+    throw new Error("Invalid upload destination");
+  }
+
+  console.log("SS3 config found", server);
+
+  return {
+    token: server.token,
+    protocol: server.secure ? "https" : "http",
+    hostname: server.host,
+    port: server.port || server.secure ? 443 : 21025,
+    path: server.ptr ? "/ptr" : "/",
+    branch: server.sim ? "sim" : "auto",
+    email: server.username,
+    password: server.password
+  };
+};
+
+const configPromise = new Promise((resolvePromise, rejectPromise) => {
+  if (!dest) {
+    console.log("No destination specified - code will be compiled but not uploaded");
+  } else {
+    try {
+      const cfg = require("./screeps.json")[dest];
+      if (cfg) {
+        return Promise.resolve(cfg);
+      }
+    } catch (error) {}
+  }
+
+  return loadSS3Config(dest).then(cfg => {
+    resolvePromise({
+      input: "src/main.ts",
+      output: {
+        file: "dist/main.js",
+        format: "cjs",
+        sourcemap: true
+      },
+
+      plugins: [
+        clear({ targets: ["dist"] }),
+        resolve(),
+        commonjs(),
+        typescript({ tsconfig: "./tsconfig.json" }),
+        screeps({ config: cfg, dryRun: cfg == null })
+      ]
+    });
+  });
+});
+
+export default configPromise;
